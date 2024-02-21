6385b5341b0173557e78149314e29c6b854ad34d
Suzy-Lee <>
2024-02-21 12:48:26 +0900

    study: PROPS in React explained
    #3
    - props로 상위에서 하위로 데이터를 보낼 수 있다.
    - props 사용 할 때, type까지 적어주는 것이 좋다.
    - props로 데이터가 생략된 경우 기본값 지정가능하다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import Button from './Button';
+import Student from './Student';
 
 function App() {
   return (
+    // NOTE: 문자열이 아닌 경우 중괄호로 묶어야함
     <>
-      <Button />
+      <Student name='Spongebob' age={30} isStudent={true} />
+      <Student name='Patrick' age={42} isStudent={false} />
+      <Student name='Squidward' age={50} isStudent={false} />
+      <Student name='Sandy' age={10} isStudent={true} />
+      <Student name='Larry' />
     </>
   );
 }
