0e189add728b7b4a13f6287e5f9bed7034e11eb5
Suzy-Lee <>
2024-02-28 15:42:00 +0900

    study: React JS how to update OBJECTS in state 🚗
    #11
    객체 업데이트 방법!

--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -1,75 +1,34 @@
 import React, { useState } from 'react';
 
 function MyComponent() {
-  const [name, setName] = useState('Guest');
-  const [quantity, setQuantity] = useState(1);
-  const [comment, setComment] = useState('');
-  const [payment, setPayment] = useState('');
-  const [shipping, setShipping] = useState('Delivery');
+  const [car, setCar] = useState({
+    year: '',
+    make: '',
+    model: '',
+  });
 
-  function handleNameChange(event) {
-    setName(event.target.value);
+  function handleYearChange(event) {
+    setCar((c) => ({ ...c, year: event.target.value }));
   }
-
-  function handleQuantityChange(event) {
-    setQuantity(event.target.value);
+  function handleMakeChange(event) {
+    setCar((c) => ({ ...c, make: event.target.value }));
   }
-
-  function handleCommentChange(event) {
-    setComment(event.target.value);
-  }
-
-  function handlePaymentChange(event) {
-    setPayment(event.target.value);
-  }
-
-  function handleShippingChange(event) {
-    setShipping(event.target.value);
+  function handleModelChange(event) {
+    setCar((c) => ({ ...c, model: event.target.value }));
   }
 
   return (
     <div>
-      <input value={name} onChange={handleNameChange} />
-      <p>Name: {name}</p>
+      <p>
+        Your favorite car is: {car.year} {car.make} {car.model}
+      </p>
 
-      <input value={quantity} onChange={handleQuantityChange} type='number' />
-      <p>Quantity: {quantity}</p>
-
-      <textarea
-        value={comment}
-        onChange={handleCommentChange}
-        placeholder='배달 완료 시, 벨 눌러주세요'
-      />
-      <p>Comment: {comment}</p>
-
-      <select value={payment} onChange={handlePaymentChange}>
-        <option value=''>카드 선택</option>
-        <option value='Visa'>Visa</option>
-        <option value='Mastercard'>Mastercard</option>
-        <option value='GiftCard'>GiftCard</option>
-      </select>
-      <p>Payment: {payment}</p>
-
-      <label>
-        <input
-          type='radio'
-          value='Pick Up'
-          checked={shipping === 'Pick Up'}
-          onChange={handleShippingChange}
-        />
-        Pick Up
-      </label><br />
-
-      <label>
-        <input
-          type='radio'
-          value='Delivery'
-          checked={shipping === 'Delivery'}
-          onChange={handleShippingChange}
-        />
-        Delivery
-      </label>
-      <p>Shipping: {shipping}</p>
+      <input type='number' value={car.year} onChange={handleYearChange} />
+      <br />
+      <input type='text' value={car.make} onChange={handleMakeChange} />
+      <br />
+      <input type='text' value={car.model} onChange={handleModelChange} />
+      <br />
     </div>
   );
 }
